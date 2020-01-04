9a688fdb497da976a9f348f3e7b006f0ff84f9d6
Haruka <haruka0314@outlook.com>
2020-01-04 23:07:47 +0800

    implement insertSort and ShellSort but not good, tomorrow will update maybe

--- Sort/Sort.py
+++ Sort/Sort.py
@@ -1,16 +1,68 @@
 def BubbleSort(array):
+    if not array:
+        return 
     for i in range(1,len(array)):
         for j in range(len(array)-i):
             if array[j] > array[j+1]:
                 temp = array[j]
                 array[j] = array[j+1]
-                array[j+1] = temp
-    return array
-            
 
 
+def SelectSort(array):
+    if not array:
+        return 
+    for i in range(0,len(array)-1):
+        min = i
+        for j in range(i+1,len(array)):
+            if array[j]<array[min]:
+                min = j
+        temp = array[min]
+        array[min] = array[i]
+        array[i] = temp
+    if array[len(array)-2] > array[len(array)-1]:
+        temp = array[len(array)-2]
+        array[len(array)-2] = array[len(array)-1]
+        array[len(array)-1] = temp
+
+
+def InsertSort(array):
+    for i in range(len(array)):
+        for j in range(i):
+            if array[j] > array[i]:
+                array.insert(j,array[i])
+                array.pop(i+1)
+                break
+
+
+def ShellSort(array):
+    gap = int(len(array)/2)
+    while gap > 1:
+        for i in range(len(array)):
+            for j in range(int(i/gap)):
+                if array[i%gap*(j+1)] > array[i]:
+                    k = i
+                    while k > i%gap*(j+1):
+                        temp = array[k]
+                        array[k] = array[k-gap]
+                        array[k-gap] = temp
+                        k -= gap
+        gap = int(gap/2)
+    gap = 2
+    for i in range(len(array)):
+        for j in range(int(i/gap)):
+            if array[i%gap*(j+1)] > array[i]:
+                k = i
+                while k > i%gap*(j+1):
+                    temp = array[k]
+                    array[k] = array[k-gap]
+                    array[k-gap] = temp
+                    k -= gap        
+    InsertSort(array)
 
 if __name__ == '__main__':
-    array = [2,4,6,37,32,17,5]
-    BubbleSort(array)
+    array = [32,46,77,11,4,6,8]
+#    BubbleSort(array)
+#    SelectSort(array)
+#    InsertSort(array)
+    ShellSort(array)
     print(array)
