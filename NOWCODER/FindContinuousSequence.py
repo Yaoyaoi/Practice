6adfa319010539f828b927cf670da5b07d69d61e
Haruka <haruka0314@outlook.com>
2020-08-07 18:04:34 +0800

    ...

--- NOWCODER/FindContinuousSequence.py
+++ NOWCODER/FindContinuousSequence.py
@@ -1,8 +1,1 @@
-class Solution:
-    def FindContinuousSequence(self, tsum):
-        for i in range(tsum//2, 1, -1):
-            if i%2:
-                
-            else:
-                if tsum%i==1/2*i:
-                    tsum
+
